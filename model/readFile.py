76716c106db6c9b1b9ccb87df74b46765369e5aa
Jesper <[jp222vq@student.lnu.se]>
2019-03-20 17:12:18 +0100

    implemented random forest regressor

--- model/readFile.py
+++ model/readFile.py
@@ -5,17 +5,23 @@ import pandas as pd
 import h5py
 import threading
 from datetime import datetime, timedelta
+from sklearn.model_selection import train_test_split
 
 log = logging.getLogger(__name__)
 
+DEP_FEATURE_NAME = 'surf_temp_sqerror'
 HDF_LOCK = threading.Lock()
 DATE_PATTERN = 'date%Y%m%d'
 TIME_PATTERN = 'time%H%M'
 KEY_PATTERN = '/{}/{}'.format(DATE_PATTERN, TIME_PATTERN)
-STORE_NAME = 'error_data_athena_t_1_1_v_1_0.h5'
+STORE_NAME = 'metro_error_data_pro_swe2018.h5'
 
-start_time = datetime(2018, 12, 7, 12, 45)
-end_time = datetime(2018, 12, 20)
+start_time = datetime(2018, 9, 29, 5)
+end_time = datetime(2018, 9, 29, 6)
+
+def define_target_features(df, dep_feature_name):
+    if dep_feature_name == 'surf_temp_sqerror':
+        df[dep_feature_name] = (df['surf_temp'] - df['stn_surf_temp']) ** 2
 
 def store_path(store_name):
     cwd = os.getcwd()
@@ -43,13 +49,24 @@ def gather_training_data(start_time, end_time):
     log.info('gathering training data from {} until {}'.format(start_time, end_time))
     ref_time = start_time
     dfs = []
-    while ref_time <= end_time:
-        dfs.append(read(ref_time))
-        ref_time += timedelta(minutes=15)
+    dfs.append(read(ref_time))
+    # while ref_time <= end_time:
+    #     dfs.append(read(ref_time))
+    #     ref_time += timedelta(minutes=15)
     df = pd.concat(dfs)
-    log.info('Training data memory usage: {}Mb'.format(df.memory_usage().sum() / 10**6))
 #    print(df.memory_usage(index=False).sum())
-    return df
+    return dfs
 
-# print(read('/date20181207/time1345/meta/values_block_1/meta', 'error_data_athena_t_1_1_v_1_0.h5'))
-print(gather_training_data(start_time, end_time))
+def train_test_splitter(df):
+    define_target_features(df, dep_feature_name=DEP_FEATURE_NAME)
+    return model
+
+
+df = gather_training_data(start_time, end_time)
+y = df['station_id']          # Split off classifications
+X = df.ix[:, 'road_cond':] # Split off features
+
+test = df.concat()
+print(test)
+# [print(key) for key in df[0].keys()]
+# print(gather_training_data(start_time, end_time))
